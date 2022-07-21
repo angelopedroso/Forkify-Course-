b5f5a912f2ab5fbef04175c83ba49443fdd5980c
Angelo <solonasusonly0@gmail.com>
2022-07-21 14:10:36 -0300

    DOM Updating

--- src/js/controller.js
+++ src/js/controller.js
@@ -18,6 +18,9 @@ const controlRecipes = async () => {
     if (!id) return;
     recipeView.renderSpinner();
 
+    //0. Update results view to mark selected search result
+    resultView.update(model.getSearchResultPage());
+
     //1. Loading recipe
     await model.loadRecipe(id);
 
@@ -60,7 +63,8 @@ const controlServings = newServings => {
   model.updateServings(newServings);
 
   //Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = () => {
